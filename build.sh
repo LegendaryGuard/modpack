f0ef47a3029dc7f1b24cda67ebedba4c1b8d4ee8
LegendaryGuard <theasixchan777@gmail.com>
2021-03-20 00:28:50 +0100

    build.sh upgraded!

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+
+if [ -e ./result/csprogs.dat -o -e ./result/menu.dat -o -e ./result/progs.dat -o -e ./result/csprogs.lno -o -e ./result/menu.lno -o -e ./result/progs.lno ]; then
+    read -r -p "The operation will compile, delete the current compiled files from result folder to replace and get new compiled ones. Are you sure you want to continue?  [Y/n] " input
+    case $input in
+    	[yY][eE][sS]|[yY])
+    rm -f ./result/*.pk3  #deletes any *.pk3 file (in that case, csprogs-*.pk3)
+    ;;
+	[nN][oO]|[nN])
+    echo "Exiting..."
+    exit 0
+    ;;
+    	*)
+    echo "Invalid input, exiting..."
+    exit 1
+    ;;
+    esac
+fi
+
 cd ${0%[\\/]*}
 set -eu
 
@@ -25,3 +43,8 @@ relpath() {
 export BUILD_MOD="$(relpath xonotic/qcsrc $PWD)"
 export XONOTIC=1
 make -C ${base}
+
+mkdir -p result
+mv -v ./xonotic/qcsrc/csprogs-*.pk3 result  #alternative file, maybe not necessary
+mv -v *.lno result
+mv -v *.dat result
